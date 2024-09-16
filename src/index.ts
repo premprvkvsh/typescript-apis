f5ee7f7a9eec4649d606800221b79a99a1c8fb2d
Prem <pprakash312003@gmail.com>
2024-09-17 00:35:13 +0530

    2) Partial API

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-//pick
-
 interface User {
-    id: number;
+    id: string;
     name: string;
+    age: string;
     email: string;
-    createdAt: Date;
-  }
+    password: string;
+};
 
-type UserProfile = Pick<User, 'name' | 'email'>;
+type UpdateProps = Pick<User, 'name' | 'age' | 'email'>
 
-const displayUserProfile = (user: UserProfile) => {
-    console.log(`Name: ${user.name}, Email: ${user.email}`);
-  };
+type UpdatePropsOptional = Partial<UpdateProps>
+
+function updateUser(updatedProps: UpdatePropsOptional) {
+    // hit the database tp update the user and perform some operations
+}
+updateUser({})
