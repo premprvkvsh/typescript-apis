97bfdaac7a37076e2c125f6c7be4ffa3506287c4
Prem <pprakash312003@gmail.com>
2024-09-17 19:24:15 +0530

    5) Exclude API

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,7 @@
 // //     email: string;
 // //     password: string;
 // // };
-const users = {
-    "abc123": { age: 23, name: "John Doe" },
-    "xyz789": { age: 34, name: "Jane Doe" },
+const handleEvent = (event) => {
+    console.log(`Handling event: ${event}`);
 };
-console.log(users["abc123"]); // Output: { id: 'abc123', name: 'John Doe' }
-users["abc123"].age;
+handleEvent("click"); // OK
--- src/index.ts
+++ src/index.ts
@@ -50,17 +50,30 @@
 
 //   users["abc123"].age;
 
-interface User {
-    id: string;
-    name: string;
-  }
+// interface User {
+//     id: string;
+//     name: string;
+//   }
   
-  // Initialize an empty Map
-  const usersMap = new Map<string, User>();
+//   // Initialize an empty Map
+//   const usersMap = new Map<string, User>();
   
-  // Add users to the map using .set
-  usersMap.set('abc123', { id: 'abc123', name: 'John Doe' });
-  usersMap.set('xyz789', { id: 'xyz789', name: 'Jane Doe' });
+//   // Add users to the map using .set
+//   usersMap.set('abc123', { id: 'abc123', name: 'John Doe' });
+//   usersMap.set('xyz789', { id: 'xyz789', name: 'Jane Doe' });
   
-  // Accessing a value using .get
-  console.log(usersMap.get('abc123')); // Output: { id: 'abc123', name: 'John Doe' }
+//   // Accessing a value using .get
+//   console.log(usersMap.get('abc123')); // Output: { id: 'abc123', name: 'John Doe' }
+
+
+
+// exclude 
+
+type EventType = 'click' | 'scroll' | 'mousemove';
+type ExcludeEvent = Exclude<EventType, 'scroll'>; // 'click' | 'mousemove'
+
+const handleEvent = (event: ExcludeEvent) => {
+  console.log(`Handling event: ${event}`);
+};
+
+handleEvent("click"); // OK
